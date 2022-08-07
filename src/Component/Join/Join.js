520efe1fcd69169a84e4a6f9e60010ebcddf65dc
goyalaayush <70816322+goyalaayush@users.noreply.github.com>
2022-08-08 01:04:50 +0530

    Update Join.js

--- src/Component/Join/Join.js
+++ src/Component/Join/Join.js
@@ -22,20 +22,7 @@ const senduser=()=>{
         <h1>Group Chat</h1>
         <input placeholder='enter name' required type="text" id='joinInput' onChange={(e)=>setuser(e.target.value)}></input>
 
-        
-        <h3 className='div2'>Choose Profile Picture<br></br><br></br>
-
-        <input
-className=' in ' 
-        type="file"
-        name="myImage"
-       
-        onChange={(event) => {
-          console.log(event.target.files[0]);
-          setSelectedImage(event.target.files[0]);
-        }}
-      
-      /></h3><br></br>
+     
       <Link  onClick={(event)=>!name?event.preventDefault():{senduser}} to="chat">  <button className='joinbtn' onClick={senduser}>Chat</button> </Link>
       </div>
   
